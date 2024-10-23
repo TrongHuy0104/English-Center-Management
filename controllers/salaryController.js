4f55090124d7ad7c2a7d63fa41ffbe4105c40d9f
congxdanh <danh.hsb13@gmail.com>
2024-10-23 21:33:17 +0700

    clean code

--- controllers/salaryController.js
+++ controllers/salaryController.js
@@ -1,15 +1,16 @@
 // controllers/feeController.js
 const Salary = require('../models/salaryModel');
 const factory = require('./handlerFactory');
+const catchAsync = require('../utils/catchAsync');
 
 // use funtcion from handlerFactory
 // exports.getAllSalaries = factory.getAll(Fee);
-exports.getSalary = factory.getOne(Fee);
-exports.createSalary = factory.createOne(Fee);
-exports.updateSalary = factory.updateOne(Fee);
-exports.deleteSalary = factory.deleteOne(Fee);
+exports.getSalary = factory.getOne(Salary);
+exports.createSalary = factory.createOne(Salary);
+exports.updateSalary = factory.updateOne(Salary);
+exports.deleteSalary = factory.deleteOne(Salary);
 exports.getAllSalaries = catchAsync(async (req, res, next) => {
-  const Salaries = await Salary.find().populate('student', 'name phone');
+  const salaries = await Salary.find().populate('teacher', 'name');
 
   res.status(200).json({
     status: 'success',
