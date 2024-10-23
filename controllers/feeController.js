566ead81a0b7f463ff920c184f0a193c6ded1676
congxdanh <danh.hsb13@gmail.com>
2024-10-23 19:20:35 +0700

    BE salaries

--- controllers/feeController.js
+++ controllers/feeController.js
@@ -1,24 +1,26 @@
-// controllers/SalaryController.js
-const Salary = require('../models/salaryModel');
+const Fee = require('../models/feeModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const Class = require('../models/classModel');
 
 // use funtcion from handlerFactory
-// exports.getAllFees = factory.getAll(Fee);
-exports.getSalary = factory.getOne(Salary);
-exports.createSalary = factory.createOne(Salary);
-exports.updateSalary = factory.updateOne(Salary);
-exports.deleteSalary = factory.deleteOne(Salary);
+exports.getAllFees = factory.getAll(Fee);
+// exports.getFee = factory.getOne(Fee);
+exports.createFee = factory.createOne(Fee);
+exports.updateFee = factory.updateOne(Fee);
+exports.deleteFee = factory.deleteOne(Fee);
 
-exports.getAllSalarys = catchAsync(async (req, res, next) => {
-  const Salaries = await Salary.find().populate('student', 'name phone');
+exports.getFee = catchAsync(async (req, res, next) => {
+  const fee = await Fee.findById(req.params.id).populate('classDetails');
+
+  if (!fee) {
+    return next(new AppError('No fee found with that ID', 404));
+  }
 
   res.status(200).json({
     status: 'success',
-    results: salaries.length,
     data: {
-      salaries,
+      fee,
     },
   });
 });
--- controllers/salaryController.js
+++ controllers/salaryController.js
@@ -3,8 +3,19 @@ const Salary = require('../models/salaryModel');
 const factory = require('./handlerFactory');
 
 // use funtcion from handlerFactory
-exports.getAllSalaries = factory.getAll(Fee);
+// exports.getAllSalaries = factory.getAll(Fee);
 exports.getSalary = factory.getOne(Fee);
 exports.createSalary = factory.createOne(Fee);
 exports.updateSalary = factory.updateOne(Fee);
 exports.deleteSalary = factory.deleteOne(Fee);
+exports.getAllSalaries = catchAsync(async (req, res, next) => {
+  const Salaries = await Salary.find().populate('student', 'name phone');
+
+  res.status(200).json({
+    status: 'success',
+    results: salaries.length,
+    data: {
+      salaries,
+    },
+  });
+});
