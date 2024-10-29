f3fe6fa96c5af636ada1a111f95e083a97488e67
DangNH <haidang12112002@gmail.com>
2024-10-29 21:06:43 +0700

    complete take attendance student, schedule, teacher profile

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const multer = require('multer');
 const teacherController = require('../controllers/teacherController');
 const classController = require('../controllers/classController');
 const salaryController = require('../controllers/salaryController');
@@ -9,7 +10,8 @@ router.get('/:teacherId/classes', classController.getClassesByTeacherId);
 router.put('/:id', teacherController.updateTeacher);
 router.get('/:id', teacherController.getTeacherById);
 router.get('/:teacherId/salary', salaryController.getSalaryByTeacherId);
-
+//const upload = multer({ dest: 'uploads/' });
+router.put('/upload/:id', teacherController.uploadAvatar);
 // Nested attendance routes under teacher with slot support
 router.put('/:teacherId/attendance/:date/:slot', attendanceController.takeAttendance);
 router.get('/:teacherId/attendance/:date/:slot', attendanceController.getAttendanceData);
