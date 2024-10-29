4fe9202712902a0c1ad9c24ad3eb874fe3899cde
Lê Nguyễn Phúc Anh <anhphuc@MacBook-Air-cua-Le.local>
2024-10-29 18:51:46 +0700

    admin class schdule

--- controllers/classController.js
+++ controllers/classController.js
@@ -1,7 +1,6 @@
 const Class = require('../models/classModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
-const sendEmail = require('../utils/email');
 
 exports.getScheduleOfStudent = catchAsync(async (req, res, next) => {
   // Tìm tất cả các lớp mà sinh viên hiện tại đang tham gia
@@ -80,6 +79,25 @@ exports.getClassById = catchAsync(async (req, res, next) => {
     status: 'success',
     data: {
       class: classWithEnrollmentStatus,
+    },
+  });
+});
+
+exports.getClassScheduleById = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  
+  const classData = await Class.findById(id)
+    .populate('teacher', 'name')
+    .populate('students._id', 'name phone')
+  
+  if (!classData) {
+    return next(new AppError('Class not found', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      class: classData,
     },
   });
 });
